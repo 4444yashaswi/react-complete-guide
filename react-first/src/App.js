2c258a065047fb0dc9f452b5003c16148b3d5f90
YASHASWI SINGH <4444yashaswi@gmail.com>
2021-03-03 02:15:57 +0530

    dynamic state change w 2 way binding

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -11,23 +11,36 @@ class App extends Component {
            
   }
 
-  clickMeHandler = () => {
+  clickMeHandler = (newName) => {
     //console.log('\'Click Me\' was clicked');
     this.setState({
         persons : [
-              {name: "Yashaswi Singh", age: 18},
+              {name: newName, age: 18},
               {name: "Random", age: 200000}
         ]
-      });
+      })
+  }
+
+  changeValueHandler = (event) => {
+    this.setState({
+      persons : [
+              {name: 'Yashaswi', age: 18},
+              {name: event.target.value, age: 20}
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>hello world</h1>
-        <button onClick = {this.clickMeHandler} >Click Me</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are watching movies</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <button onClick = {this.clickMeHandler.bind(this,'Yashaswi Singh')} >Click Me</button>
+        <Person name={this.state.persons[0].name}
+         age={this.state.persons[0].age}
+         clicked={this.clickMeHandler.bind(this,'WAiWAi')}>My hobbies are watching movies</Person>
+        <Person name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         valChange={this.changeValueHandler} />
       </div>
     );
   }
--- react-first/src/Person/Person.js
+++ react-first/src/Person/Person.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const person = (props) => {
 	return <div>
-		<p>I'm {props.name} and i'm {props.age} years old.</p>
+		<p onClick={props.clicked}>I'm {props.name} and i'm {props.age} years old.</p>
 		<p>{props.children}</p>
+		<input type="text" onChange={props.valChange} value={props.name}/>
 	</div>
 }
 
