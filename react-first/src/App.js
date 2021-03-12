980a70972d4d358f592caee308441d22ee638da8
YASHASWI SINGH <4444yashaswi@gmail.com>
2021-03-13 03:20:23 +0530

    Partially working dynamic 'persons part' in the app added

--- react-first/src/App.js
+++ react-first/src/App.js
@@ -5,14 +5,32 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons : [
-              {name: "Yashaswi", age: 18},
-              {name: "Random", age: 20}
+              {id: "ab1", name: "Yashaswi", age: 18},
+              {id: "ab2", name: "Kushagra", age: 21},
+              {id: "ab3", name: "Random", age: 20}
         ],
     showPerson: false   
   }
 
+  nameChangedHandler = (event , id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.targate.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
+
   deletePersonHandler = (personIndex) => {
-    const persons=this.state.persons;
+    const persons=[...this.state.persons];
     persons.splice(personIndex,1);
     this.setState({persons: persons})
   }
@@ -32,8 +50,10 @@ class App extends Component {
             this.state.persons.map((person , index) => {
               return <Person
               clicked = {() => this.deletePersonHandler(index)}
-              name= {person.name}
-              age= {person.age} />
+              name = {person.name}
+              age = {person.age} 
+              key = {person.id}
+              changed = {(event) => this.nameChangedHandler(event , person.id)} />
             })
           }
           </div>
--- react-first/src/Person/Person.js
+++ react-first/src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
 	return <div className = 'Person'>
 		<p onClick={props.clicked}>I'm {props.name} and i'm {props.age} years old.</p>
 		<p>{props.children}</p>
-		<input type="text" onChange={props.valChange} value={props.name}/>
+		<input type="text" onChange={props.changed} value={props.name}/>
 	</div>
 }
 
