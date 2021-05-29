b9b524387c1a9e55ae11d3e0bc41fc2899e1d305
YASHASWI SINGH <4444yashaswi@gmail.com>
2021-05-30 00:39:00 +0530

    completed module 3

--- expense-record/src/App.js
+++ expense-record/src/App.js
@@ -1,10 +1,39 @@
-import './App.css';
+import Expenses from './components/Expenses'
 
 function App() {
+  const items = [
+  {
+    id: 'e1',
+    title: 'Pencil',
+    amount: 2,
+    date: new Date(2021, 0, 1),
+  },
+  {
+    id: 'e2',
+    title: 'Book',
+    amount: 200,
+    date: new Date(),
+  },
+  {
+    id: 'e3',
+    title: 'sofa',
+    amount: 20000,
+    date: new Date(),
+  },
+  {
+    id: 'e4',
+    title: 'Pen',
+    amount: 20,
+    date: new Date(),
+  }
+  ];
+
+
   return (
-    <h1 className="App">
-      Hello world!
-    </h1>
+    <div>
+      <h1>Hello world!</h1>
+      <Expenses items={items}/>
+    </div>
   );
 }
 
