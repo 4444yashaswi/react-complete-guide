df24efd49cd9205f2046182f956c4b4ed211512a
YASHASWI SINGH <4444yashaswi@gmail.com>
2021-06-15 20:05:43 +0530

    added authentication in users

--- users/src/components/Users/UserForm.js
+++ users/src/components/Users/UserForm.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import './UserForm.css';
 import Button from '../UI/Button';
 
 const UserForm = props => {
 	return <div className = 'user-form__container'>
-		<div className = 'user-form__username'>
-			<lable>Username</lable>
-			<input />
-		</div>
-		<div className = 'user-form__age'>
-			<lable>Age (Years)</lable>
-			<input />
-		</div>
-		<Button type = 'submit'>Add User</Button>
+		<form onSubmit = {props.onSubmitHandler}>
+			<div className = 'user-form__username'>
+				<lable htmlFor = 'username'>Username</lable>
+				<input id = 'username' value = {props.username} onChange = {props.usernameChangeHandler} />
+			</div>
+			<div className = 'user-form__age'>
+				<lable htmlFor = 'age'>Age (Years)</lable>
+				<input id = 'age'  value = {props.age} onChange = {props.ageChangeHandler}/>
+			</div>
+			<Button type = 'submit'>Add User</Button>
+		</form>
 	</div>
 };
 
